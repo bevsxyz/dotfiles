6e00181f17971894d63910913c31fb75acbfbb41
Bevan Stanely <bevanstanely@iisc.ac.in>
2021-11-01 15:54:12 +0530

    Update dot_config/nvim/lua/statusline.lua

--- dot_config/nvim/lua/statusline.lua
+++ dot_config/nvim/lua/statusline.lua
@@ -1,51 +1,32 @@
- local mode_map = {
-	['n'] = 'normal ',
-	['no'] = 'n·operator pending ',
-	['v'] = 'visual ',
-	['V'] = 'v·line ',
-	[''] = 'v·block ',
-	['s'] = 'select ',
-	['S'] = 's·line ',
-	[''] = 's·block ',
-	['i'] = 'insert ',
-	['R'] = 'replace ',
-	['Rv'] = 'v·replace ',
-	['c'] = 'command ',
-	['cv'] = 'vim ex ',
-	['ce'] = 'ex ',
-	['r'] = 'prompt ',
-	['rm'] = 'more ',
-	['r?'] = 'confirm ',
-	['!'] = 'shell ',
-	['t'] = 'terminal '
-}
+local git = require('statusline.git')
+local utils = require('utils')
+local M = {}
 
-local function mode()
-	local m = vim.api.nvim_get_mode().mode
-	if mode_map[m] == nil then return m end
-	return mode_map[m]
-end
-
+-- set highlights for statusline sections
 vim.api.nvim_exec(
 [[
-  hi PrimaryBlock   ctermfg=06 ctermbg=00
-  hi SecondaryBlock ctermfg=08 ctermbg=00
-  hi Blanks   ctermfg=07 ctermbg=00
+  hi PrimaryBlock ctermfg=06 ctermbg=00
+  hi SecondaryBlock   ctermfg=07 ctermbg=00
+  hi Blanks   ctermfg=08 ctermbg=00
+  hi GitClean ctermfg=02 ctermbg=00
+  hi GitDirty ctermfg=01 ctermbg=00
 ]], false)
 
+function M.statusline()
+  local stl = {
+    '%#PrimaryBlock#',
+    '%f',
+    '%#Blanks#',
+    '%m',
+    '%#SecondaryBlock#',
+    ' '..git.git_branch,
+    '%=',
+    '%#SecondaryBlock#',
+    '%l,%c ',
+    '%#PrimaryBlock#',
+    '%{&filetype}',
+  }
+  return table.concat(stl)
+end
 
-local stl = {
-  '%#PrimaryBlock#',
-  mode(),
-  '%#SecondaryBlock#',
-  '%#Blanks#',
-  '%f',
-  '%m',
-  '%=',
-  '%#SecondaryBlock#',
-  '%l,%c ',
-  '%#PrimaryBlock#',
-  '%{&filetype}',
-}
-
-vim.o.statusline = table.concat(stl)
+return M
